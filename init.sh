f79480f1885a2c0095c45da517c13a2a1bff6ede
Ethan <ethan@pop-os.localdomain>
2019-12-12 19:27:29 -0700

    fdisk code
    added code from super user that allows using fdisk to format disks

--- init.sh
+++ init.sh
@@ -37,6 +37,39 @@ let efiPartition=${disk}1
 let swapParitition=${disk}2
 let rootParitition=${disk}3
 
+# stolen from "How to create and format partition using a bash script" from superUser
+# to create the partitions programatically (rather than manually)
+# we're going to simulate the manual input to fdisk
+# The sed script strips off all the comments so that we can 
+# document what we're doing in-line with the actual commands
+# Note that a blank line (commented as "defualt" will send a empty
+# line terminated with a newline to take the fdisk default.
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+  +500M # 500 MB boot parttion
+  n # new partition
+  p # primary partition
+  2 # partion number 2
+    # default, start immediately after preceding partition
+  +2G # 2 Gigabyte swap partition
+  n # new partition
+  p # primary partition
+    # default, start immediately after preceding partition
+    # default, extend partition to end of disk
+  a # make a partition bootable
+  3 # bootable partition is partition 1 -- /dev/sda1
+  p # print the in-memory partition table
+  w # write the partition table
+  q # and we're done
+EOF
+
+
+
+
 
 
 echo "==========================================="
