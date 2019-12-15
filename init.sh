b0a154b046e3c14b7932604edfb64271154078b8
Ethan <ethan@pop-os.localdomain>
2019-12-14 18:51:36 -0700

    changed commands for fdisk

--- init.sh
+++ init.sh
@@ -45,26 +45,25 @@ let rootParitition=${disk}3
 # Note that a blank line (commented as "defualt" will send a empty
 # line terminated with a newline to take the fdisk default.
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
-  o # clear the in memory partition table
+  d # delete partition
+    # confirm
+  d # delete partition (maybe if not just continues)
+    # confirm
+  d # delete parittion
+    # confirm
   n # new partition
-  p # primary partition
-  1 # partition number 1
+    # partition number 1
     # default - start at beginning of disk 
   +500M # 500 MB boot parttion
   n # new partition
-  p # primary partition
-  2 # partion number 2
+    # partion number 2
     # default, start immediately after preceding partition
   +2G # 2 Gigabyte swap partition
   n # new partition
-  p # primary partition
     # default, start immediately after preceding partition
     # default, extend partition to end of disk
-  a # make a partition bootable
-  3 # bootable partition is partition 1 -- /dev/sda1
   p # print the in-memory partition table
   w # write the partition table
-  q # and we're done
 EOF
 
 
