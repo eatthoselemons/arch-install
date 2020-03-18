050d39b7d4379efb094929a322fed4f9102edb6c
ethan <preevete@gmail.com>
2020-03-18 14:00:35 -0600

    added termite config

--- firstStartup.sh
+++ firstStartup.sh
@@ -17,3 +17,20 @@ Section "InputClass"
   Option "AccelerationThreshold" "0"
 EndSection
 EOF
+
+# setup termite
+echo "installing termite terminal emulator"
+sudo pacman -S --noconfirm termite
+
+mkdir -p ~/git
+cd ~/git
+
+git clone https://github.com/khamer/base16-termite.git
+
+cp ~/git/base16-termite/themes/base16-monokai.config ~/.config/termite/config
+
+cat << EOF >> ~/.config/termite/config
+[options]
+font = Monospace 13
+scrollback_lines = 100000
+EOF
