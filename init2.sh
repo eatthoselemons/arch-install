2134085270c4444fcc5b28a958ae4a9bde22975c
ethan <ethan@evergreenatm.io>
2020-03-19 12:15:53 -0600

    removed 'LANG' from the locale generation due to new locale syntax

--- init2.sh
+++ init2.sh
@@ -93,7 +93,7 @@ echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 locale-gen
 
 touch /etc/locale.conf
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "en_US.UTF-8 UTF-8" > /etc/locale.conf
 
 touch /etc/hostname
 echo $hostname > /etc/hostname
