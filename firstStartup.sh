0de42144adbfef2a7623ddce7b9b409ba0b511bf
ethan <ethan@evergreenatm.io>
2020-03-22 16:44:55 -0600

    mistakenly commited init2.sh with the info for the changes to firstStartup
    the added echo's for the user to know what they are doing as well as the check for the existing git directory were changes to firstStartup not init2.sh

--- firstStartup.sh
+++ firstStartup.sh
@@ -12,12 +12,17 @@ sudo pacman -S --noconfirm xorg xorg-xinit
 echo "installing xmonad"
 sudo pacman -S --noconfirm xmonad xmonad-contrib xterm
 
+# install git
+echo "installing git"
+sudo pacman -S --noconfirm git
+
 # allow 'startx' to start the xmonad display manager
 echo "#!/bin/bash" > ~/.xinitrc
 echo "exec xmonad" >> ~/.xinitr
 
 # disable mouse acceleration
-cat << EOF > /etc/X11/xorg.conf.d/no-mouse-acceleration.conf
+echo "disabling mouse acceleration"
+sudo bash -c "cat << EOF > /etc/X11/xorg.conf.d/no-mouse-acceleration.conf
 Section "InputClass"
   Identifier "MyMouse"
   MatchIsPointer "yes"
@@ -26,22 +31,33 @@ Section "InputClass"
   Option "AccelerationDenominator" "1"
   Option "AccelerationThreshold" "0"
 EndSection
-EOF
+EOF"
 
 # install termite
 echo "installing termite terminal emulator"
 sudo pacman -S --noconfirm termite
 
-mkdir -p ~/git
-cd ~/git
 
 # get termite color schemes
+echo "cloning termite color schemes"
+echo "making git directory"
+mkdir -p ~/git
+cd ~/git
+if [[ -d ~/git/base16-termite ]]
+then
+  echo "removing existing termite color scheme directories"
+  rm -rf ~/git/base16-termite
+  rmdir ~/git/base16-termite
+fi
 git clone https://github.com/khamer/base16-termite.git
 
 # move monokai termite theme to the termite config dir
+echo "creating termite config dir and moving monokai into that dir"
+mkdir -p ~/.config/termite/config
 cp ~/git/base16-termite/themes/base16-monokai.config ~/.config/termite/config
 
 # increase default termite size from 9 (default) to 13
+echo "increasing default terminal font size"
 cat << EOF >> ~/.config/termite/config
 [options]
 font = Monospace 13
