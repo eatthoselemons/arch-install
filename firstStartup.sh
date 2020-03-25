e177e74a46a30a65cd25b1580dd70d74e55c5b41
ethan <preevete@gmail.com>
2020-03-24 22:42:58 -0600

    added instruction messages to start the various display managers and added installing udisk2

--- firstStartup.sh
+++ firstStartup.sh
@@ -12,13 +12,28 @@ sudo pacman -S --noconfirm xorg xorg-xinit dmenu
 echo "installing xmonad"
 sudo pacman -S --noconfirm xmonad xmonad-contrib xterm
 
+# install other display manager
+echo "installing deepin"
+sudo pacman -S --noconfirm deepin
+
 # install other useful programs
 echo "installing other programs, git, firefox etc"
-sudo pacman -S --noconfirm git firefox
+sudo pacman -S --noconfirm git firefox udisks2
 
 # allow 'startx' to start the xmonad display manager
-echo "#!/bin/bash" > ~/.xinitrc
-echo "exec xmonad" >> ~/.xinitrc
+cat << EOF > ~/.xinitrc
+case "$2" in
+  xmonad)
+    exec xmonad
+    ;;
+  deepin)
+    exec startdde
+    ;;
+  *)
+    echo "failure"
+    ;;
+esac
+EOF
 
 # disable mouse acceleration
 echo "disabling mouse acceleration"
@@ -62,3 +77,7 @@ cat << EOF >> ~/.config/termite/config
 font = Monospace 13
 scrollback_lines = 100000
 EOF
+
+echo "If you would like to use eatthoselemons linux config run eatthoselemonsLinuxConfig.sh"
+echo "To start a display manager run 'startx {display manager}'"
+echo "so you could run 'startx xmonad' to start the xmonad display manager"
