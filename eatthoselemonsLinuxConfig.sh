04e924c80062a6fb848e71025769aac9343ce406
ethan <preevete@gmail.com>
2020-04-29 22:28:52 -0600

    added the stuff from linux-config into the eatthoselemons config

--- eatthoselemonsLinuxConfig.sh
+++ eatthoselemonsLinuxConfig.sh
@@ -5,19 +5,31 @@ trap "${trap_msg}" ERR
 
 mkdir -p ~/git
 
+if [[ -d $HOME/git/sddm-deepin ]];
+then
+	rm -rf "$HOME/git/sddm-deepin"
+fi
+
 # install deepin
-git clone https://github.com/Match-Yang/sddm-deepin.git ~/git
+git clone https://github.com/Match-Yang/sddm-deepin.git ~/git/sddm-deepin
 
 cd ~/git/sddm-deepin
 
 bash install.sh
 
+# install usefull utilities
+sudo pacman --noconfirm -S ripgrep
 
 # install my linux config
 # dependencies
-sudo pacman -S universal-ctags neovim
+sudo pacman --noconfirm -S universal-ctags neovim
 
-git clone https://github.com/eatthoselemons/linux-config ~/.
+if [[ -d $HOME/linux-config ]];
+then
+	rm -rf "$HOME/linux-config"
+fi
+
+git clone https://github.com/eatthoselemons/linux-config ~/linux-config
 
 cat << EOF > ~/.bashrc
 if [ -f $HOME/linux-config/extra-bash ];
